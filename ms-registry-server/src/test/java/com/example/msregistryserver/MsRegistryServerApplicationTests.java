fd7e3c7b109d932ffe999e8b15d3deab7a9aeadf
stefano159jk <stefanochura@gmail.com>
2024-04-01 10:05:48 -0500

    ms-registry-server

--- ms-registry-server/src/test/java/com/example/msregistryserver/MsRegistryServerApplicationTests.java
+++ ms-registry-server/src/test/java/com/example/msregistryserver/MsRegistryServerApplicationTests.java
@@ -2,8 +2,6 @@ package com.example.msregistryserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 
 @SpringBootTest
 class MsRegistryServerApplicationTests {
